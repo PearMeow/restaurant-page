78089ff380d5569a55b98b2b121c438f8fc4851f
Perry Huang <perryhuang2015@gmail.com>
2025-07-20 11:07:02 -0400

    Add common config and separate dev & production configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,38 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const common = require("./webpack.common.js");
+const { merge } = requre("webpack-merge");
 
-
-module.exports = {
+module.exports = merge(common, {
     mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            }
-        ],
-    },
-};
+});
